20ac45f8b996463f2a20ae3ac32e3d57b3472777
Ajay-2007 <iit2017040@iiita.ac.in>
2020-08-05 16:51:44 +0530

    Added files for training chatbots with RL

---
+++ 14.Training Chatbots with RL/data/get_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x
+
+[ ! -f cornell_movie_dialogs_corpus.zip ] && wget http://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip
+[ ! -d "cornell movie-dialogs corpus" ] && unzip -x cornell_movie_dialogs_corpus.zip && rm -rf __MACOSX
+[ ! -d cornell ] && mv "cornell movie-dialogs corpus" cornell
